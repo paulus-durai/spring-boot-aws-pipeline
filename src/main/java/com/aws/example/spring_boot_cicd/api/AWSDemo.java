b7dd2e69e2358fe85817b3549edd6eae880ec92e
paulusjebadur.ambro <paulusjebadur.ambro@hcltech.com>
2025-03-25 22:16:36 +0530

    test

---
+++ src/main/java/com/aws/example/spring_boot_cicd/api/AWSDemo.java
@@ -0,0 +1,17 @@
+package com.aws.example.spring_boot_cicd.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AWSDemo {
+	
+	@GetMapping
+	public String awsDemoPinpeline()
+	{
+		return "AWS Demo test";
+	}
+	
+	
+
+}
---
+++ src/test/java/com/aws/example/spring_boot_cicd/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.example.spring_boot_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
